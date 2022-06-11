7d39511560ac363a277ccd6e673f5e8cc5d745aa
Yves <yalikalfic@yahoo.com>
2022-06-11 15:10:12 -0400

    adding cypress

--- bloglistFrontend/.eslintrc.js
+++ bloglistFrontend/.eslintrc.js
@@ -6,7 +6,11 @@ module.exports = {
     'jest/globals': true,
     'cypress/globals': true,
   },
-  extends: ['eslint:recommended', 'plugin:react/recommended'],
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:cypress/recommended',
+  ],
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
