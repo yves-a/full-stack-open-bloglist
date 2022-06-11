b095533bf0b0c561c397417e33fbcafbfbdbfc7b
Yves <yalikalfic@yahoo.com>
2022-06-11 15:35:31 -0400

    fixing unit tests

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const config = require('./utils/config')
 const logger = require('./utils/logger')
 
 const server = http.createServer(app)
-
 server.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`)
 })
